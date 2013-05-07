615735a68968d38d9bbe8470e4d132850223dbc3
James Humphry <git@binecho.co.uk>
2013-05-07 19:18:03 +0100

    Added functions to find direct connections from a station
    Added functions to find the direct fixed links from a given station
    on a given day and time and the direct scheduled services. A
    timetable should be extracted into a temporary table using
    mca.get_full_timetable for the latter.

---
+++ sql/mca_get_direct_connections.sql
@@ -0,0 +1,40 @@
+﻿DROP FUNCTION IF EXISTS mca.get_direct_connections(timetable varchar, station char(7), depart time);
+
+CREATE FUNCTION mca.get_direct_connections(timetable varchar, station char(7), depart time)
+RETURNS TABLE ( 	
+		location char(7),
+		earliest_arrival time,
+		train_uid char(6)
+		) 
+AS $B$
+BEGIN
+
+-- The CTE valid_trains identifies all the train schedules that depart from the given station
+-- after the given time. The query valid_routes then makes a list of all the subsequent
+-- stations on those train schedules. The window function partitions the results by location and
+-- ranks the results by arrival time, with before-midnight trains first.
+-- The outer query then only selects the earliest train arrival time for each location.
+
+RETURN QUERY EXECUTE format('
+	SELECT location, scheduled_arrival, train_uid
+	FROM (
+		WITH valid_trains AS(
+			SELECT train_uid, loc_order
+			FROM %I 
+			WHERE location = $1
+			AND scheduled_departure > $2
+			)
+		SELECT 	location, 
+			scheduled_arrival,
+			valid_trains.train_uid,
+			rank() OVER (PARTITION BY location ORDER BY timetable.xmidnight, scheduled_arrival ASC) AS foo
+		FROM %I AS timetable	
+			INNER JOIN valid_trains 
+				ON (timetable.train_uid = valid_trains.train_uid 
+					AND timetable.loc_order > valid_trains.loc_order)
+		WHERE scheduled_arrival IS NOT NULL ) AS valid_routes
+	WHERE foo = 1;', timetable, timetable) 
+		USING station, depart;
+END;
+$B$
+LANGUAGE 'plpgsql' ;
--- sql/mca_get_train_timetable.sql
+++ sql/mca_get_train_timetable.sql
@@ -1,51 +1,53 @@
-﻿CREATE OR REPLACE FUNCTION mca.get_train_timetable (train_uid character(6), timetable_date date)
-RETURNS TABLE (	train_uid character(6),
-		stp_indicator character(1),
-		loc_order integer,
-		xmidnight boolean,
-		location character(7),
-		station_name character(30),
-		scheduled_arrival time without time zone,
-		scheduled_departure time without time zone,
-		scheduled_pass time without time zone,
-		platform character(3) ) AS $$
+﻿DROP FUNCTION IF EXISTS mca.get_train_timetable (train_uid character(6), timetable_date date);
 
-	WITH bs AS (
-		SELECT train_uid, date_runs_from, stp_indicator
-		FROM mca.basic_schedule
-		WHERE 	train_uid = $1 AND
-			date_runs_from <= $2 AND
-			date_runs_to >= $2 AND
-			days_run[EXTRACT(ISODOW FROM $2)]
-		ORDER BY (stp_indicator COLLATE "C") ASC
-		LIMIT 1
-		), locations AS (
-		SELECT train_uid, date_runs_from, stp_indicator,
-			loc_order, FALSE AS xmidnight, LEFT(location, 7) AS location,
-			NULL AS scheduled_arrival, scheduled_departure, NULL AS scheduled_pass,
-			platform
-		FROM mca.origin_location
-		WHERE train_uid = $1 AND date_runs_from <= $2
-		UNION
-		SELECT train_uid, date_runs_from, stp_indicator,
-			loc_order, xmidnight, LEFT(location, 7) AS location,
-			scheduled_arrival, scheduled_departure, scheduled_pass,
-			platform
-		FROM mca.intermediate_location
-		WHERE train_uid = $1 AND date_runs_from <= $2
-		UNION
-		SELECT train_uid, date_runs_from, stp_indicator,
-			loc_order, xmidnight, LEFT(location, 7) AS location,
-			scheduled_arrival, NULL AS scheduled_departure, NULL AS scheduled_pass,
-			platform
-		FROM mca.terminating_location
-		WHERE train_uid = $1 AND date_runs_from <= $2
-		)
-	SELECT train_uid, stp_indicator, loc_order, xmidnight, location, station_name, scheduled_arrival,
-		scheduled_departure, scheduled_pass, platform
-	FROM locations
-		INNER JOIN bs USING (train_uid, date_runs_from, stp_indicator)
-		LEFT JOIN msn.station_detail AS sd ON (sd.tiploc_code = location)
-	ORDER BY loc_order
-	
+CREATE OR REPLACE FUNCTION mca.get_train_timetable (train_uid character(6), timetable_date date)
+RETURNS TABLE ( train_uid character(6),
+        stp_indicator character(1),
+        loc_order integer,
+        xmidnight boolean,
+        location character(7),
+        station_name character(30),
+        scheduled_arrival time without time zone,
+        scheduled_departure time without time zone,
+        scheduled_pass time without time zone,
+        platform character(3) ) AS $$
+
+    WITH bs AS (
+        SELECT train_uid, date_runs_from, stp_indicator
+        FROM mca.basic_schedule
+        WHERE   train_uid = $1 AND
+            date_runs_from <= $2 AND
+            date_runs_to >= $2 AND
+            days_run[EXTRACT(ISODOW FROM $2)]
+        ORDER BY (stp_indicator COLLATE "C") ASC
+        LIMIT 1
+        ), locations AS (
+        SELECT train_uid, date_runs_from, stp_indicator,
+            loc_order, FALSE AS xmidnight, LEFT(location, 7) AS location,
+            NULL AS scheduled_arrival, scheduled_departure, NULL AS scheduled_pass,
+            platform
+        FROM mca.origin_location
+        WHERE train_uid = $1 AND date_runs_from <= $2
+        UNION
+        SELECT train_uid, date_runs_from, stp_indicator,
+            loc_order, xmidnight, LEFT(location, 7) AS location,
+            scheduled_arrival, scheduled_departure, scheduled_pass,
+            platform
+        FROM mca.intermediate_location
+        WHERE train_uid = $1 AND date_runs_from <= $2
+        UNION
+        SELECT train_uid, date_runs_from, stp_indicator,
+            loc_order, xmidnight, LEFT(location, 7) AS location,
+            scheduled_arrival, NULL AS scheduled_departure, NULL AS scheduled_pass,
+            platform
+        FROM mca.terminating_location
+        WHERE train_uid = $1 AND date_runs_from <= $2
+        )
+    SELECT train_uid, stp_indicator, loc_order, xmidnight, location, station_name, scheduled_arrival,
+        scheduled_departure, scheduled_pass, platform
+    FROM locations
+        INNER JOIN bs USING (train_uid, date_runs_from, stp_indicator)
+        LEFT JOIN msn.station_detail AS sd ON (sd.tiploc_code = location)
+    ORDER BY loc_order
+
 $$ LANGUAGE SQL;
