33fd5c5f1fcad741f989d2449b4a61258d7303f2
James Humphry <git@binecho.co.uk>
2016-01-16 23:06:37 +0000

    Fix bug in util.isocron and reformat
    The date passed into util.isocron would be used to create the correct
    day's timetable, but in the call to iterate_reachable the date
    field was filled out with a constant. This had a limited impact
    because only the link with the ALF tables was affected. This has been
    fixed and the file has also been reformatted.

--- sql/util_isochron.sql
+++ sql/util_isochron.sql
@@ -1,43 +1,43 @@
-﻿DROP FUNCTION IF EXISTS util.isochron(	station char(7),
-					depart time,
-					timetable_date date);
+﻿DROP FUNCTION IF EXISTS util.isochron(station char(7),
+                                      depart time,
+                                      timetable_date date);
 
-CREATE FUNCTION  util.isochron(	station char(7),
-				depart time,
-				timetable_date date)
+CREATE FUNCTION  util.isochron(station char(7),
+                               depart time,
+                               timetable_date date)
 RETURNS TABLE (
-		location char(7),
-		delay double precision,
-		easting integer,
-		northing integer
-		)
+        location char(7),
+        delay double precision,
+        easting integer,
+        northing integer
+        )
 AS $IC$
 DECLARE
 
 BEGIN
-	CREATE TEMP TABLE isochron_tt AS
-		SELECT * FROM mca.get_full_timetable(timetable_date);
-	INSERT INTO isochron_tt SELECT * FROM ztr.get_full_timetable(timetable_date);
+    CREATE TEMP TABLE isochron_tt AS
+        SELECT * FROM mca.get_full_timetable(timetable_date);
+    INSERT INTO isochron_tt SELECT * FROM ztr.get_full_timetable(timetable_date);
 
-	CREATE INDEX idx_isochron_tt_loc ON isochron_tt (location);
-	CREATE INDEX idx_isochron_tt_uid ON isochron_tt (train_uid);
-	ANALYZE isochron_tt;
+    CREATE INDEX idx_isochron_tt_loc ON isochron_tt (location);
+    CREATE INDEX idx_isochron_tt_uid ON isochron_tt (train_uid);
+    ANALYZE isochron_tt;
 
-	RETURN QUERY SELECT ir.location,
-			EXTRACT(hours from (ir.earliest_arrival - depart)) +
-			EXTRACT(minutes from (ir.earliest_arrival - depart)) / 60.0 AS delay,
-			sd.easting*100-1000000 AS easting,
-			sd.northing*100-6000000 AS northing
-		FROM util.iterate_reachable(	'isochron_tt',
-						station,
-						depart,
-						'2013-05-09',
-						10) AS ir
-			INNER JOIN msn.station_detail AS sd
-				ON (ir.location = sd.tiploc_code)
-		ORDER BY ir.earliest_arrival;
+    RETURN QUERY SELECT ir.location,
+            EXTRACT(hours from (ir.earliest_arrival - depart)) +
+            EXTRACT(minutes from (ir.earliest_arrival - depart)) / 60.0 AS delay,
+            sd.easting*100-1000000 AS easting,
+            sd.northing*100-6000000 AS northing
+        FROM util.iterate_reachable('isochron_tt',
+                                    station,
+                                    depart,
+                                    timetable_date,
+                                    10) AS ir
+            INNER JOIN msn.station_detail AS sd
+                ON (ir.location = sd.tiploc_code)
+        ORDER BY ir.earliest_arrival;
 
-	DROP TABLE isochron_tt;
+    DROP TABLE isochron_tt;
 END;
 $IC$
 LANGUAGE 'plpgsql';
